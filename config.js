5e3e6db5726731dc3f822ad83f36f13012b38c30
CorneliaInvestment10 <info@corneliainvestmentgroup.org>
2026-05-29 17:47:42 -0400

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// Google Apps Script Web App URL
-const LEAD_CAPTURE_ENDPOINT = "https://script.google.com/macros/s/AKfycbxwUq_-XVh7iB6rLfjGVJrKcJcneYaIexFFDinPz-r2y5XkOS6VFvhsPacNkJ4T0LYb/exec";
-
-// Replace this with your NBC tracking link before publishing.
-const NBC_REFERRAL_LINK = "https://www.nationalbusinesscapital.com/apply-now/?ref=140622073002";
